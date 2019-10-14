8637af6c1c1b018fbc91184a38f1ae9380989857
Jim Ngo <jimngo2013@gmail.com>
2019-10-14 09:54:06 -0700

    SEO updates

---
+++ javascript/service.js
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+// Slide Show                                                                 /
+///////////////////////////////////////////////////////////////////////////////
+
+onload = slideshow;
+
+function slideshow() {
+    setInterval(function () {
+        nextSlide('service-ss');
+        nextSlide('home-valuation-ss');
+        nextSlide('dpa-ss');
+        nextSlide('refinance-ss');
+        nextSlide('sfr-ss');
+    }, 2000);
+}
+
+function nextSlide(id) {
+    var elems = document.getElementById(id).getElementsByClassName('slide');
+    elems.index = elems.index || 0;
+    elems[elems.index].className = 'slide';
+    elems.index = (elems.index + 1) % elems.length;
+    elems[elems.index].className = 'slide showing';
+}
